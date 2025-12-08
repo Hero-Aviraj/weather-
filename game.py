98912717154267c9d341c0eccf854d8103f4ce12
Haraprasad Mondal <haraprasadmondal232@gmail.com>
2025-12-08 15:17:39 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,1 @@
-order=input("enter the order")
-print("still im not sure what i have to do ")
-print("enter your gaming name ")
-if(order=="food"):
-    print("ok sir thank you")
-else:
-    print("thank you")    
+
